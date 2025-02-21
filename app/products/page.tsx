ed9fadb7848f1b82ce7fbea7242fc8a99c803db1
Sarang Khadse <sarangkhadse021@gmail.com>
2025-02-22 02:30:36 +0530

    Working version restored

--- app/products/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Products() {
-  return (
-    <div className="p-4">
-      <h1 className="text-xl font-bold">Products List</h1>
-      <p>Product list will go here...</p>
-    </div>
-  );
-}
